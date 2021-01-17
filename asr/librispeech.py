63ab32fb24ad339248fbb38183bae558e61de1b5
morgan <morgan_kelly.wixted@dfki.de>
2021-01-17 23:08:29 +0100

    removed no_grad(), fixed batch size

--- asr/librispeech.py
+++ asr/librispeech.py
@@ -114,7 +114,7 @@ LSTM
 '''
 
 train_loader = data.DataLoader(dataset=train_dataset,
-                                batch_size=5,
+                                batch_size=10,
                                 shuffle=True,
                                 collate_fn=lambda x: data_processing(x)
                                 )
@@ -127,47 +127,43 @@ criterion = nn.CTCLoss(blank=28).to(device)
 def train(model, device, train_loader, criterion, optimizer, scheduler, epoch):
     model.train()
     data_len = len(train_loader.dataset)
-    with torch.no_grad():
-        for batch_idx, _data in enumerate(train_loader):
-            spectrograms, labels, input_lengths, label_lengths = _data
-            spectrograms, labels = spectrograms.to(device), labels.to(device)
+    for batch_idx, _data in enumerate(train_loader):
+        spectrograms, labels, input_lengths, label_lengths = _data
+        spectrograms, labels = spectrograms.to(device), labels.to(device)
 
 
-            optimizer.zero_grad()
-            print("model")
-            output = model(spectrograms)
+        optimizer.zero_grad()
+        print("model")
+        output = model(spectrograms)
 
-            output = F.log_softmax(output, dim=2)
+        output = F.log_softmax(output, dim=2)
 
-            output = output.transpose(0, 1)
+        output = output.transpose(0, 1)
 
 
-            loss = criterion(output, labels, input_lengths, label_lengths)
-            loss.requires_grad = True
-            loss.backward()
+        loss = criterion(output, labels, input_lengths, label_lengths)
+        loss.backward()
 
-            optimizer.step()
-            scheduler.step()
-           # gc.collect()
-            #torch.cuda.empty_cache()
+        optimizer.step()
+        scheduler.step()
 
-            if batch_idx % 100 == 0 or batch_idx == data_len:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(spectrograms), data_len,
-                        100. * batch_idx / len(train_loader), loss.item()))
+        if batch_idx % 100 == 0 or batch_idx == data_len:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(spectrograms), data_len,
+                    100. * batch_idx / len(train_loader), loss.item()))
 
-             # save the model
-                '''
-                The learnable parameters in PyTorch are contained in the model's parameters.
-                A state_dict is a Python dictionary object that maps each layer to its parameters tensor.
-                A state_dict can be easily saved, updated, altered, and restored.
-                '''
-                #print("Model's state_dict:")
+            # save the model
+            '''
+            The learnable parameters in PyTorch are contained in the model's parameters.
+            A state_dict is a Python dictionary object that maps each layer to its parameters tensor.
+            A state_dict can be easily saved, updated, altered, and restored.
+            '''
+            #print("Model's state_dict:")
             # for param_tensor in model.state_dict():
-                # print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+            # print(param_tensor, "\t", model.state_dict()[param_tensor].size())
 
-                print("saving model")
-                torch.save(model.state_dict(), path_to_model)
+            print("saving model")
+            torch.save(model.state_dict(), path_to_model)
 
 rnn_dim = 512
 hidden_dim = 512
