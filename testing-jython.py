b76bb97a7152627a8a0abf6cf7b4bd9077aa89d4
ariabee <abrown72@uvm.edu>
2021-01-18 13:54:50 +0100

    begin testing jython and a simple dialogue for use in DialogOS

---
+++ testing-jython.py
@@ -0,0 +1,6 @@
+# Example code from jython.org
+
+from java.lang import System # Java import
+
+print('Running on Java version: ' + System.getProperty('java.version'))
+print('Unix time from Java: ' + str(System.currentTimeMillis()))
