4249aaabd17a16dbf52e7c8869d98d48bd27ece2
morgan <morgan_kelly.wixted@dfki.de>
2021-01-20 23:39:14 +0100

    started trying to implement the asr plugin for dialog os in Jython

---
+++ asr-plugin/src/main/jython/test_node.py
@@ -0,0 +1,5 @@
+from com.clt.diamant.graph import Node
+from javax.swing import JLabel, BorderFactory, JTabbedPane, JPanel
+from java.awt import GridBagLayout, GridBagConstraints, Insets
+from com.clt.diamant.gui import NodePropertiesDialog
+
