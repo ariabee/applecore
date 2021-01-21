4e7f470d0720477b49a956812a66b8f4719cc0cf
ariabee <abrown72@uvm.edu>
2021-01-21 14:17:51 +0100

    added temporary command instructions to testing-jython.py

--- testing-jython.py
+++ testing-jython.py
@@ -1,4 +1,10 @@
 # Example code from jython.org
+# Note for running jython if not in PATH:
+# 	In Terminal, try running:
+#	`java -jar [location of jython.jar in home directory] [name of python w/ java file]`
+#	For example:
+#	`java -jar ~/jython2.7.2/jython.jar "testing-jython.py"`
+
 
 from java.lang import System # Java import
 
