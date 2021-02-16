9c38be5cbd7a10e296d0b8d09fe204e386c8732b
morgan <morgan_kelly.wixted@dfki.de>
2021-02-16 17:45:28 +0100

    nothing works yet but I want to play with another model now

--- asr/librispeech.py
+++ asr/librispeech.py
@@ -39,7 +39,7 @@ train_local_dataset = "/home/morgan/Documents/saarland/fourth_semester/lap_softw
 
 path_to_model = "/local/morganw/speech_recognition_saved_models/server.pt"
 
-path_to_local_model = "/home/morgan/Documents/saarland/fourth_semester/lap_software_project/project/librispeech_models/laptop.pt"
+path_to_local_model = "/home/morgan/Documents/saarland/fourth_semester/lap_software_project/project/librispeech_models/libritts_laptop.pt"
 #check to see if the datasets have already been downloaded. If not, download them.
 #if os.path.isfile(train_file):
    # print("Train dataset exists")
@@ -72,6 +72,30 @@ Data Preprocessing: Extract feature from the spectrographs and map the transcrip
 '''
 text_transform = TextTransform()
 
+def maxPadLength(labels):
+    max_pad_length = 0
+    for i in labels:
+        if len(i) >= max_pad_length:
+            max_pad_length = len(i)
+    return max_pad_length
+
+def padLabels(labels, label_lengths):
+    max_pad_length = maxPadLength(labels)
+    print("labels list")
+    print(labels)
+    for i in labels:
+        print("length of labels")
+        print(i)
+        if i <= max_pad_length:
+            print("max pad length - len(i_length)")
+            print(max_pad_length - i)
+            i = i + [0] * (max_pad_length - i)
+            print("new label lengths")
+            print(i)
+            label_lengths.append(torch.tensor(i))
+    print("label_lengths list")
+    print(label_lengths)
+
 
 def data_processing(data):
     spectrograms = []
@@ -87,20 +111,35 @@ def data_processing(data):
                     #print(filename)
                     waveform, sample_rate = torchaudio.load(wav_file)
                     spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
-                    spectrograms.append(spec)
-                    input_lengths.append(spec.shape[0] // 2)
+                    #print("spec shape")
+                    #print(spec.shape)
+                    #spectrograms.append(spec)
+                    #input_lengths.append(spec.shape[0] // 2)
                 if filename.endswith(".normalized.txt"):
                     transcription_file = open(os.path.join(working_directory, filename))
                     transcription_file = transcription_file.read()
                     transcription_file = re.sub("[^\w\s]", "", transcription_file)
                     # create the labels by taking the preprocessed transcriptions and using the text_transform class to map the characters to numbers
-                    label = text_transform.text_to_int(transcription_file.lower())
-                    labels.append(label)
+                    label = torch.tensor(text_transform.text_to_int(transcription_file.lower()))
+                    #labels.append(label)
+                   # label_lengths.append(len(label))
 
-   max_pad_length = 0
-   for i in labels:
-       # if len(i) > max_pad_length:
-         #   max_pad_length = len(i)
+        spectrograms.append(spec)
+        labels.append(label)
+        print(spec.shape[0])
+        print("numpy ceiling")
+        print(int(np.ceil(spec.shape[0] / 2)))
+        input_lengths.append(int(np.ceil(spec.shape[0] / 2)))
+        label_lengths.append(len(label))
+        print("input lengths")
+        print(input_lengths)
+
+
+    #padding = maxPadLength(labels)
+
+    #padded_labels = padLabels(labels, label_lengths)
+
+
 
     #max_pad_length
    # for label_length in labels:
@@ -108,55 +147,41 @@ def data_processing(data):
         #    label_length = label_length + [0]*(max_pad_length - len(label_length))
          #   label_lengths.append(torch.tensor(label_length))
 
-    #targets = torch.stack(label_lengths)
-    #print(targets.shape)
+
     spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
-    #print("spectrograms shape")
-    #print(spectrograms.shape)
-    #labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
-    #print(labels)
-   # for label in labels:
-      #  print(type(label))
-    #max_pad_length = 191
-   # for label in labels:
-       # for length in label.shape:
-            #if length > max_pad_length:
-              #  max_pad_length = length
-
-    #labels = torch.nn.functional.pad(labels, (1,1))
+   # print("spectrograms shape")
+   # print(spectrograms.shape)
+    labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
     #print(labels.shape)
-   # print("labels")
-    #for label in labels:
-      #  print(len(label))
-   # print(labels.shape)
-   # print("padded labels")
-   # print(labels.shape)
 
-    return spectrograms, targets
 
-data_processing(train_local_dataset)
+
+
+    return spectrograms, labels, input_lengths, label_lengths
+
+#data_processing(train_local_dataset)
 
 '''
 LSTM
 '''
 
 train_loader = data.DataLoader(dataset=train_local_dataset,
-                                batch_size=191,
+                                batch_size=10,
                                 shuffle=True,
                                 collate_fn=lambda x: data_processing(x)
                                 )
 use_cuda = torch.cuda.is_available()
 torch.manual_seed(7)
 device = torch.device("cuda" if use_cuda else "cpu")
-#criterion = nn.CTCLoss(blank=28).to(device)
-criterion = nn.NLLLoss().to(device)
+criterion = nn.CTCLoss(blank=28).to(device)
+#criterion = nn.NLLLoss().to(device)
 
 
 def train(model, device, train_loader, criterion, optimizer, scheduler, epoch):
     model.train()
     data_len = len(train_loader.dataset)
     for batch_idx, _data in enumerate(train_loader):
-        spectrograms, labels = _data
+        spectrograms, labels, input_lengths, label_lengths = _data
         print("spectrograms")
         print(spectrograms.shape)
         print("labels")
@@ -170,7 +195,10 @@ def train(model, device, train_loader, criterion, optimizer, scheduler, epoch):
         print("output of model")
         print(output.shape)
 
-        loss = criterion(torch.log(output), labels)
+        print("input lengths")
+        print(input_lengths)
+
+        loss = criterion(output, labels, input_lengths, label_lengths)
         loss.backward()
 
         optimizer.step()
@@ -191,8 +219,8 @@ def train(model, device, train_loader, criterion, optimizer, scheduler, epoch):
             # for param_tensor in model.state_dict():
             # print(param_tensor, "\t", model.state_dict()[param_tensor].size())
 
-            #print("saving model")
-           # torch.save(model.state_dict(), path_to_local_model)
+            print("saving model")
+            torch.save(model.state_dict(), path_to_local_model)
 
 rnn_dim = 512
 hidden_dim = 512
@@ -215,9 +243,9 @@ scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=5e-4,
                                               epochs=10,
                                              anneal_strategy='linear')
 
-epochs = 1
-#for epoch in range(1, epochs + 1):
-    #train(model, device, train_loader, criterion, optimizer, scheduler, epoch)
+epochs = 10
+for epoch in range(1, epochs + 1):
+    train(model, device, train_loader, criterion, optimizer, scheduler, epoch)
 
 
 
