c1023493db24f0fa870e9eeb4249666581fff887
morgan <morgan_kelly.wixted@dfki.de>
2021-02-24 16:28:33 +0100

    modified preprocessing code-hopefully now we don't have exploding gradients and can run more data with one model

--- asr/librispeech.py
+++ asr/librispeech.py
@@ -54,6 +54,8 @@ def data_processing(data):
     labels = []
     input_lengths = []
     label_lengths = []
+    flac_files = []
+    transcriptions = []
     for top_directory in os.listdir(train_local_dataset):
         for second_directory in os.listdir(os.path.join(train_local_dataset, top_directory)):
             working_directory = os.path.join(train_local_dataset, top_directory, second_directory)
@@ -61,34 +63,43 @@ def data_processing(data):
                 if filename.endswith(".flac"):
                     #get path of flac file
                     flac_file = os.path.join(working_directory, filename)
-                    #use torch audio to load the flac file to get the waveform and sample rate
-                    waveform, sample_rate = torchaudio.load(flac_file)
-                    #transform the waveform using the train_audio_transforms as defined above
-                    spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
-                    #append all of the newly created spectrograms to a list
-                    spectrograms.append(spec)
-                    #create a list of input lengths for the CTC loss function
-                    input_lengths.append(spec.shape[0] // 2)
+                    flac_files.append(flac_file)
                 if filename.endswith(".txt"):
                     transcription_file = open(os.path.join(working_directory, filename))
                     transcription_file = transcription_file.read().split("\n")
                     for transcription in transcription_file:
                         # preprocess using a regex to remove the identifying labels and just have the transcribed speech
                         transcription_file = re.sub("[\d-]", "", transcription)
-                        # create the labels by taking the preprocessed transcriptions and using the text_transform class to map the characters to numbers
-                        label = torch.Tensor(text_transform.text_to_int(transcription_file.lower()))
-                        labels.append(label)
-                        #create a list of label lengths for the CTC loss function
-                        label_lengths.append(len(label))
+                        transcriptions.append(transcription_file)
+
+    for ff in flac_files:
+        # use torch audio to load the flac file to get the waveform and sample rate
+        waveform, sample_rate = torchaudio.load(ff)
+        # transform the waveform using the train_audio_transforms as defined above
+        spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
+        # append all of the newly created spectrograms to a list
+        spectrograms.append(spec)
+        # create a list of input lengths for the CTC loss function
+        input_lengths.append(spec.shape[0] // 2)
+
+    for tf in transcriptions:
+        # create the labels by taking the preprocessed transcriptions and using the text_transform class to map the characters to numbers
+        label = torch.Tensor(text_transform.text_to_int(tf.lower()))
+        labels.append(label)
+        # create a list of label lengths for the CTC loss function
+        label_lengths.append(len(label))
 
     #pad both the spectrograms and labels
     spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
-
+    #print("spectrograms")
+   # print(spectrograms.shape)
     labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
+    #print("labels")
+    #print(labels.shape)
 
     return spectrograms, labels, input_lengths, label_lengths
 
-data_processing(train_local_dataset)
+#data_processing(train_local_dataset)
 #spectrograms, labels, input_lengths, label_lengths = data_processing(train_local_dataset)
 #print("labels")
 #print(labels)
@@ -113,8 +124,12 @@ def train(model, device, train_loader, criterion, optimizer, epoch):
     data_len = len(train_loader.dataset)
     for batch_idx, _data in enumerate(train_loader):
         spectrograms, labels, input_lengths, label_lengths = _data
-        print("input lengths")
-        print(len(input_lengths))
+       # print("input lengths")
+        #print(len(input_lengths))
+        #print("batch index")
+        #print(batch_idx)
+        print("spectrograms")
+        print(spectrograms.shape)
 
         spectrograms, labels = spectrograms.to(device), labels.to(device)
         print("labels")
@@ -144,8 +159,8 @@ def train(model, device, train_loader, criterion, optimizer, epoch):
             A state_dict is a Python dictionary object that maps each layer to its parameters tensor.
             A state_dict can be easily saved, updated, altered, and restored.
             '''
-            print("saving model")
-            torch.save(model.state_dict(), path_to_model)
+            #print("saving model")
+           # torch.save(model.state_dict(), path_to_model)
 
 #parameters for the model
 rnn_dim = 512
@@ -171,9 +186,9 @@ optimizer = optim.Adam(model.parameters(), lr=5e-4)
                                              #anneal_strategy='linear')
 
 #train the model
-epochs = 30
-#for epoch in range(1, epochs + 1):
-   # train(model, device, train_loader, criterion, optimizer, epoch)
+epochs = 1
+for epoch in range(1, epochs + 1):
+    train(model, device, train_loader, criterion, optimizer, epoch)
 
 
 
