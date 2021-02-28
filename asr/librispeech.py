fixed exploding gradient problem - also can train with entire data set now

asr/librispeech.py
@@ -9,6 +9,8 @@ import torchaudio
 import numpy as np
 from text_transform import TextTransform
 from model import SpeechRecognitionModel
+import warnings
+warnings.filterwarnings("ignore")
 '''
 This script serves as a foundational aspect of development for the ASR unit for a voice controlled video game
 for the Language, Action, and Perception software project. 
@@ -20,9 +22,11 @@ The cited website is primarily serving as the model for the data and also the de
 '''
 torch.set_printoptions(profile="full")
 #path for LibriSpeech training dataset
-train_dataset = "/local/morganw/librispeech/LibriSpeech/train-server"
+#train_dataset = "/local/morganw/librispeech/LibriSpeech/train-server"
+train_url="train-clean-100"
 
-train_local_dataset = "/home/morgan/Documents/saarland/fourth_semester/lap_software_project/project/corpora/LibriSpeech/train-laptop/"
+train_local_dataset = torchaudio.datasets.LIBRISPEECH("/home/morgan/Documents/saarland/fourth_semester/lap_software_project/project/corpora/LibriSpeech", url=train_url, download=True)
+#train_local_dataset = "/home/morgan/Documents/saarland/fourth_semester/lap_software_project/project/corpora/LibriSpeech/train-clean-100"
 
 path_to_model = "/local/morganw/applecore/speech_commands_model/librispeech/librispeech_server.pt"
 
@@ -54,48 +58,19 @@ def data_processing(data):
     labels = []
     input_lengths = []
     label_lengths = []
-    flac_files = []
-    transcriptions = []
-    for top_directory in os.listdir(train_local_dataset):
-        for second_directory in os.listdir(os.path.join(train_local_dataset, top_directory)):
-            working_directory = os.path.join(train_local_dataset, top_directory, second_directory)
-            for filename in os.listdir(working_directory):
-                if filename.endswith(".flac"):
-                    #get path of flac file
-                    flac_file = os.path.join(working_directory, filename)
-                    flac_files.append(flac_file)
-                if filename.endswith(".txt"):
-                    transcription_file = open(os.path.join(working_directory, filename))
-                    transcription_file = transcription_file.read().split("\n")
-                    for transcription in transcription_file:
-                        # preprocess using a regex to remove the identifying labels and just have the transcribed speech
-                        transcription_file = re.sub("[\d-]", "", transcription)
-                        transcriptions.append(transcription_file)
-
-    for ff in flac_files:
-        # use torch audio to load the flac file to get the waveform and sample rate
-        waveform, sample_rate = torchaudio.load(ff)
-        # transform the waveform using the train_audio_transforms as defined above
+    for (waveform, _, utterance, _, _, _) in data:
         spec = train_audio_transforms(waveform).squeeze(0).transpose(0, 1)
-        # append all of the newly created spectrograms to a list
         spectrograms.append(spec)
-        # create a list of input lengths for the CTC loss function
-        input_lengths.append(spec.shape[0] // 2)
-
-    for tf in transcriptions:
-        # create the labels by taking the preprocessed transcriptions and using the text_transform class to map the characters to numbers
-        label = torch.Tensor(text_transform.text_to_int(tf.lower()))
+        label = torch.Tensor(text_transform.text_to_int(utterance.lower()))
         labels.append(label)
-        # create a list of label lengths for the CTC loss function
+        input_lengths.append(spec.shape[0]//2)
         label_lengths.append(len(label))
 
-    #pad both the spectrograms and labels
+
     spectrograms = nn.utils.rnn.pad_sequence(spectrograms, batch_first=True).unsqueeze(1).transpose(2, 3)
-    #print("spectrograms")
-   # print(spectrograms.shape)
+
     labels = nn.utils.rnn.pad_sequence(labels, batch_first=True)
-    #print("labels")
-    #print(labels.shape)
+
 
     return spectrograms, labels, input_lengths, label_lengths
 
@@ -107,16 +82,17 @@ def data_processing(data):
 '''
 LSTM
 '''
-
-train_loader = data.DataLoader(dataset=train_dataset,
+use_cuda = torch.cuda.is_available()
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+train_loader = data.DataLoader(dataset=train_local_dataset,
                                 batch_size=20,
                                 shuffle=True,
-                                collate_fn=lambda x: data_processing(x)
-                                )
-use_cuda = torch.cuda.is_available()
+                                collate_fn=lambda x: data_processing(x),
+                                **kwargs)
+
 torch.manual_seed(7)
 device = torch.device("cuda" if use_cuda else "cpu")
-criterion = nn.CTCLoss(blank=28, zero_infinity=True).to(device)
+criterion = nn.CTCLoss(blank=28).to(device)
 
 
 def train(model, device, train_loader, criterion, optimizer, epoch):
@@ -124,16 +100,8 @@ def train(model, device, train_loader, criterion, optimizer, epoch):
     data_len = len(train_loader.dataset)
     for batch_idx, _data in enumerate(train_loader):
         spectrograms, labels, input_lengths, label_lengths = _data
-       # print("input lengths")
-        #print(len(input_lengths))
-        #print("batch index")
-        #print(batch_idx)
-        print("spectrograms")
-        print(spectrograms.shape)
 
         spectrograms, labels = spectrograms.to(device), labels.to(device)
-        print("labels")
-        print(labels.shape)
 
         optimizer.zero_grad()
         print("model")
@@ -146,7 +114,7 @@ def train(model, device, train_loader, criterion, optimizer, epoch):
         loss.backward()
 
         optimizer.step()
-        #scheduler.step()
+        scheduler.step()
 
         if batch_idx % 100 == 0 or batch_idx == data_len:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
@@ -159,8 +127,8 @@ def train(model, device, train_loader, criterion, optimizer, epoch):
             A state_dict is a Python dictionary object that maps each layer to its parameters tensor.
             A state_dict can be easily saved, updated, altered, and restored.
             '''
-            #print("saving model")
-           # torch.save(model.state_dict(), path_to_model)
+            print("saving model")
+            torch.save(model.state_dict(), path_to_model)
 
 #parameters for the model
 rnn_dim = 512
@@ -180,13 +148,13 @@ model = nn.DataParallel(model)
 model.to(device)
 #define optimizer and scheduler
 optimizer = optim.Adam(model.parameters(), lr=5e-4)
-#scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=5e-4,
-                                             # steps_per_epoch=int(len(train_loader)),
-                                             # epochs=10,
-                                             #anneal_strategy='linear')
+scheduler = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=5e-4,
+                                             steps_per_epoch=int(len(train_loader)),
+                                             epochs=10,
+                                             anneal_strategy='linear')
 
 #train the model
-epochs = 1
+epochs = 10
 for epoch in range(1, epochs + 1):
     train(model, device, train_loader, criterion, optimizer, epoch)
 