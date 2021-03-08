6a3542129d77438ad0b2c518f472abecf0a0d563
ariabee <abrown72@uvm.edu>
2021-03-08 11:35:40 +0100

    moves code into move_if_clear_path(), comments out action_queue attempts

--- gamefiles/agent.py
+++ gamefiles/agent.py
@@ -34,13 +34,14 @@ class Agent(pg.sprite.Sprite):
         self.vel = vec(0, 0)
         self.position = vec(x, y)
         self.dest = vec_dest(x, y)
+        self.previous_pos = vec_prev(x, y)
         self.instruction = ""
         self.orientation = "front" # left, right, front, back
         self.name = "Young Apple"
         self.knowledge = Knowledge(self)
         self.transcript = Transcript()
-        self.current_actions = []  # working memory
-        self.previous_pos = vec_prev(x, y)
+        self.action_queue = []  # working memory of current, complete list of actions e.g. [[1],[0],[2]]
+        self.responses = []
 
 
     def turn(self, direction):
@@ -49,6 +50,11 @@ class Agent(pg.sprite.Sprite):
         """
         # self.image.blit(self.img_0/90/180/270, ((x, y)))
         pass
+
+    def give_name(self, new_name):
+        self.name = new_name
+        mapped_meaning = self.knowledge.lexicon()["you"]
+        self.knowledge.add_to_lexicon(new_name, mapped_meaning)
 
     def blink(self):
         """
@@ -64,6 +70,28 @@ class Agent(pg.sprite.Sprite):
             self.image = self.images[0]
             self.blinks = False
             self.start_time = end_time
+
+    def move_if_clear_path(self):
+        """
+        Checks whether the agent can continue moving to its destination 
+        on a clear x and y path. If clear, moves the agent closer to its destination.
+        """
+        #TODO: adjust math.isclose to also check for x and y board limit value?
+        clear_path = not math.isclose(self.position.x, self.dest.x, rel_tol=1e-09, abs_tol=0.5) or \
+                     not math.isclose(self.position.y, self.dest.y, rel_tol=1e-09, abs_tol=0.5)
+
+        if clear_path:
+            self.knowledge.set_direction()
+            #print(self.position, self.dest)
+            self.position += self.vel * self.game.dt
+            self.hit_rect.centerx = self.position.x
+            collide_with_walls(self, self.game.walls, 'x')
+            self.hit_rect.centery = self.position.y
+            collide_with_walls(self, self.game.walls, 'y')
+            self.rect.center = self.hit_rect.center
+
+        #print("checked for clear path: "+str(clear_path))
+        return clear_path
             
     def listen_attempt(self):
         #UNCOMMENT FOR SPEECH VERSION
@@ -71,6 +99,7 @@ class Agent(pg.sprite.Sprite):
         if keys[pg.K_SPACE]:
             self.vel = vec(0, 0)
             self.dest = vec_dest(self.position.x, self.position.y)
+            self.action_queue = []
             with sr.Microphone() as source:
                 try:
                     audio = r.listen(source, timeout=5)
@@ -106,15 +135,6 @@ class Agent(pg.sprite.Sprite):
 
         return self.instruction
 
-
-    def give_name(self, new_name):
-        self.name = new_name
-        mapped_meaning = self.knowledge.lexicon()["you"]
-        self.knowledge.add_to_lexicon(new_name, mapped_meaning)
-
-    def store_parsed_actions(self, parsed_actions):
-        self.current_actions = parsed_actions
-
     def interpret(self):
         """
         The Agent processes the instruction (temporarily stored in self) into
@@ -123,7 +143,6 @@ class Agent(pg.sprite.Sprite):
         return: composition, the recognized string of words
         return: actions, the list of corresponding actions
         """
-
         composition = ""
         actions = []
         unknowns = ""
@@ -153,12 +172,11 @@ class Agent(pg.sprite.Sprite):
                 actions.append(lexicon[word])
                 # print(actions)
 
-        # self.store_parsed_actions(actions)
-
         return (composition, actions)
 
     def compose_actions(self, actions):
         """
+        Currently composes actions into list of single actions. Returns list.
         (Here is where semantics are helpful...:))
         Composes the actions into a meaningful sequence. 
         Returns the composed action sequence (a list of integer lists) e.g. [[1],[0,3,1],[2]]
@@ -173,22 +191,26 @@ class Agent(pg.sprite.Sprite):
         #     for action in single_actions:
         #         print(str(type(action)))
                
-
         # print("actions: " + str(actions))
-        return actions
 
-    def try_actions(self, parsed_actions):
+        single_actions = []
+        for action_list in actions:
+            for action in action_list:
+                single_actions.append([action]) # note that action is still inside a list e.g. [1]
+        return single_actions
+
+    def try_actions(self, action_queue):
         """
         Execute the retrieved action functions.
         """
         responses = []
-        
-        for actions in parsed_actions:
+
+        for actions in action_queue:
             for action in actions:
                 action_response = self.knowledge.actions[action]()  # do the action, get the response
 
                 responses.append(action_response)  # todo: update responses to be specific to user's words
-            # (through a function parameter or through function itself)
+                                                   # (through a function parameter or through function itself)
 
         return (responses)
 
@@ -204,35 +226,64 @@ class Agent(pg.sprite.Sprite):
         composition, parsed_actions = self.interpret()
 
         # 2) Compose the actions into a meaningful sequence and save to working memory
-        self.current_actions = self.compose_actions(parsed_actions)
+        self.action_queue = self.compose_actions(parsed_actions)
         
         # 3) Save the instruction and current actions to transcript
         if self.instruction:
-            self.transcript.store(self.instruction, self.current_actions)
+            self.transcript.store(self.instruction, self.action_queue)
 
         # 4) Try the actions and collect the responses
-        responses = self.try_actions(parsed_actions)
+        responses = self.try_actions(self.action_queue)
+        #responses = ""
 
         return (composition, responses)
+
+    # def try_action(self):
+    #     """
+    #     Execute the next action in the queue and return the response.
+    #     """
+    #     response = ""
+    #     if self.action_queue:
+    #         action = self.action_queue[0][0]
+    #         response = self.knowledge.actions[action]()
+    #         print("next action: " + str(action) + ", response: " + str(response))
+        
+    #     return response
+
+    # def try_action_in_queue(self):
+    #     """
+    #     """
+    #     responses = []
+    #     if self.action_queue: # if there are still actions to complete
+    #         print("action_queue: " + str(self.action_queue))
+    #         action = self.action_queue[0][0]
+    #         response = self.knowledge.actions[action]() # do action, store the response
+    #         print("action: " + str(action) + ", response: " + str(response))
+
+    #         still_moving = self.move_if_clear_path() # continue completing the action
+            
+    #         if not still_moving: # if the action is complete, add the response and pop the action off the queue
+    #             responses.append(response)
+    #             print("responses: " + str(responses))
+    #             popped = self.action_queue.pop(0)
+    #             print("popped: " + str(popped))
 
 
     def update(self):
         self.listen_attempt()
+        #self.try_action_in_queue()
+
         self.blink()
         self.rect = self.image.get_rect()
         self.rect.center = self.position
 
-        #TODO: put the below code into a method, call the method inside update
-        if not math.isclose(self.position.x, self.dest.x, rel_tol=1e-09, abs_tol=0.5) or \
-                not math.isclose(self.position.y, self.dest.y, rel_tol=1e-09, abs_tol=0.5):
-            self.knowledge.set_direction()
-            #print(self.position, self.dest)
-            self.position += self.vel * self.game.dt
-            self.hit_rect.centerx = self.position.x
-            collide_with_walls(self, self.game.walls, 'x')
-            self.hit_rect.centery = self.position.y
-            collide_with_walls(self, self.game.walls, 'y')
-            self.rect.center = self.hit_rect.center
+        # response = self.try_action()
+        # still_moving = self.move_if_clear_path()
+        # if not still_moving and self.action_queue:
+        #     self.responses.append(response)
+        #     self.action_queue.pop(0)
+
+        self.move_if_clear_path()
 
         self.transcript.save()
 
